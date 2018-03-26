2201c433c0da9c86b50ca431ca55b6de816b34e0
Valeria Antonova <valeria.antonia001@gmail.com>
2018-03-26 21:24:55 +0600

    чуть поправили

--- Core/View.php
+++ Core/View.php
@@ -35,7 +35,7 @@ class View
         static $twig = null;
 
         if ($twig === null) {
-            $loader = new \Twig_Loader_Filesystem('..App/Views');
+            $loader = new \Twig_Loader_Filesystem('../App/Views');
             $twig = new \Twig_Environment($loader);
         }
 
