678eaf88e6fef2598b08c3c551052cc6f9bcae02
Valeria Antonova <valeria.antonia001@gmail.com>
2018-03-27 23:14:24 +0600

    Добавили вызов метода представления в контроллер постов. Все работает!

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use \Core\View;
+
 //Posts controller
 // PHP version 7.0.28
 
@@ -12,9 +14,7 @@ class Posts extends \Core\Controller
 
     public function indexAction()
     {
-        echo 'Hello from the index action in the Posts controller!';
-        echo '<p> Query string parameters: <pre>' .
-         htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
+        View::renderTemplate('Posts/index.html');
     }
 
 //Show the add new page
